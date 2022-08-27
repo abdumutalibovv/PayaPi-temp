c0a300b73d7c726b6c25df8ceaa3d469a8230f51
abdumutalibov123 <aozodbekh81@gmail.com>
2022-08-28 02:21:12 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// --->BU YERGA CLASS NOMLARINI KIRGISASZ<----//
+let navMenu = document.querySelector("")
+let navClose = document.querySelector("")
+let mobile = document.querySelector("")
+let nav = document.querySelector("")
+
+
+
+// --->BU YERGA CLASS NOMLARINI KIRGISASZ<----//
+
+// navMenu.addEventListener("click", function(){
+//     .style.display = "block";
+//     .style.display = "block"
+//     .style.display = "none";
+//     .style.display = "flex"
+// })
+
+// navClose.addEventListener("click", function(){
+//     .style.display = "none";
+//     .style.display = "none"
+//     .style.display = "block";
+// })
